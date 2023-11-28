e0cd7c7ef2e7b74bce93307e7408af6f17c14353
musllim <muslimuwitondanishema@gmail.com>
2023-11-28 11:59:42 +0200

    add altair chart

--- unemployment.py
+++ unemployment.py
@@ -1,11 +1,13 @@
 import pandas as pd
 import streamlit as st
 import plotly.express as px
+import altair as alt
 from st_btn_select import st_btn_select
+from pie import filter
 
 drop_columns = ["Urban", "Rural"]
-selection = "Chart"
 select = None
+RWANDAN_POPULATION = 13000000
 
 
 @st.cache_data
@@ -13,7 +15,7 @@ def load_data():
     return pd.read_excel("RLFS_2022_Data_clean.xlsx", sheet_name="Table 0").iloc[1:]
 
 
-selection = st_btn_select(("Chart", "Data"))
+selection = st_btn_select(("Charts", "Data", "Customize chart"))
 
 
 def render(data, select=""):
@@ -29,11 +31,14 @@ def main():
     data = load_data()
 
     if selection == "Data":
-        select = st.radio("Filter data based on your need:", ("Residence", "gender"))
+        select = st.sidebar.radio(
+            "Filter data based on your need:", ("Residence", "gender")
+        )
         st.write(render(data, select))
-    elif selection == "Chart":
-        select = st.radio("Filter data based on your need:", ("Percentage(%)", "Total"))
-
+    elif selection == "Charts":
+        select = st.sidebar.radio(
+            "Filter data based on your need:", ("Percentage(%)", "Total")
+        )
         processed_data = render(data)
         fig = px.bar(
             data_frame=processed_data,
@@ -49,6 +54,37 @@ def main():
             fig.update_traces(texttemplate="%{y:.2f}%", textposition="outside")
 
         st.plotly_chart(fig)
+    if selection == "Customize chart":
+        df_sheet_4 = pd.read_excel("RLFS_2022_Data_clean.xlsx", sheet_name="Table 4")
+
+        selected_area = st.sidebar.radio("Filter by Area", ["Rural", "Urban"])
+        select = st.sidebar.radio("Filter gender:", ("Male", "Female"))
+        melted_df = pd.melt(
+            df_sheet_4,
+            id_vars=["Category", "Yrs"],
+            value_vars=[selected_area, select],
+        )
+
+        chart = (
+            alt.Chart(melted_df)
+            .mark_bar()
+            .encode(
+                x=alt.X("Category:N", title="Category"),
+                y=alt.Y(
+                    "sum(value):Q",
+                    title="Total",
+                    scale=alt.Scale(domain=[0, RWANDAN_POPULATION]),
+                ),
+                color=alt.Color("Yrs:N", title="Years"),
+            )
+            .properties(width=alt.Step(60), height=500)
+            .configure_axis(
+                titlePadding=20,
+                labelPadding=10,
+            )
+        )
+
+        st.altair_chart(chart, use_container_width=True)
 
 
 main()
