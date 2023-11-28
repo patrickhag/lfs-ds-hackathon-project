0cc4c841d891792287022c1442ed0ae1237be321
musllim <muslimuwitondanishema@gmail.com>
2023-11-28 20:21:52 +0200

    fix titles

--- education_fields.py
+++ education_fields.py
@@ -1,27 +1,41 @@
 import streamlit as st
 from streamlit_folium import folium_static
 import pandas as pd
-from st_btn_select import st_btn_select
 import plotly.express as px
 
-selection = st_btn_select(("Chart", "Data"))
+Chart, Data = st.tabs(["Charts", "Table of ata"])
 df_sheet_2 = pd.read_excel("RLFS_2022_Data_clean.xlsx", sheet_name="Table 2")
 dd = ["Male", "Female", "All"]
 select = st.sidebar.radio("Filter data based on gender:", dd)
-if selection == "Data":
+with Data:
     if select == "All":
         st.write(df_sheet_2)
     else:
         st.write(df_sheet_2.drop(columns=[select, "Total"]))
-elif select != "All":
-    dd.pop()
-    fig = px.bar(
-        data_frame=df_sheet_2,
-        x=select,
-        y="Field of Education",
-        height=500,
-        width=800,
-        title="Youth unemployment rate by education level",
-    )
-    fig.update_traces(texttemplate="%{x:,}", textposition="outside")
-    st.plotly_chart(fig)
+with Chart:
+    if select == "All":
+        fig = px.bar(
+            data_frame=df_sheet_2,
+            x=["Male", "Female"],
+            y="Field of Education",
+            height=500,
+            width=800,
+            title="Youth unemployment rate by education level",
+        )
+        fig.update_traces(texttemplate="%{x:,}", textposition="outside")
+        st.plotly_chart(fig)
+    else:
+        fig = px.bar(
+            data_frame=df_sheet_2,
+            x=select,
+            y="Field of Education",
+            height=500,
+            width=800,
+            title="Youth unemployment rate by education level",
+        )
+        fig.update_traces(texttemplate="%{x:,}", textposition="outside")
+        fig.update_layout(
+            xaxis=dict(scaleanchor="y", scaleratio=1),
+            yaxis=dict(scaleanchor="x", scaleratio=1),
+        )
+        st.plotly_chart(fig)
